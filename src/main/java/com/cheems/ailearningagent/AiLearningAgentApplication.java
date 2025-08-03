40f64c5f8910f6538dc823ef6efd21992f747e17
cheems <2323864130@qq.com>
2025-08-03 15:17:17 +0800

    初始化项目，引入依赖

---
+++ src/main/java/com/cheems/ailearningagent/AiLearningAgentApplication.java
@@ -0,0 +1,13 @@
+package com.cheems.ailearningagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLearningAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiLearningAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cheems/ailearningagent/AiLearningAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheems.ailearningagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLearningAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
